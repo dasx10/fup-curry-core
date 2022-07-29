7b19f27ded7ea50c1deb4ac2e6b117413ce574fa
dasx10ls <dasx10@gmail.com>
2022-07-30 02:46:08 +0300

    refactor: 💡 removve test

--- index.ts
+++
@@ -1,8 +0,0 @@
-import curryCore from "./source";
-import { CurryCore } from "./index.d"
-
-const curry = curryCore as CurryCore
-const a  = <X extends string, Y extends string>(x: X, y: Y) => x + y as `${X}${Y}`
-const a1 = curry(a, 'ds' as const, '' as const);
-
-const a2 = curry((x: number, y: number) => (x + y), 2, 3);
